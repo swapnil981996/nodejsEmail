91a6275c578d4ab0988036df82a541f1306bea22
swapnil <swapnil.g@boppotechnologies.com>
2020-07-08 16:21:27 +0530

    initial-commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+app.set("view engine","jade")
+
+app.get('/send-email', function(req, res){
+   console.log(req.query.toEmail)
+   const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+   if(req.query.toEmail)
+   {
+      if(emailRegexp.test(req.query.toEmail))
+      {
+         res.render('Email',{ 'msg': 'mail sent' });
+      }
+      else
+      {
+         res.render('Email',{ 'msg': 'invalid email address' });
+      }
+      
+   }
+   else
+   {
+      res.render('Email',{ 'msg': '' });
+   }
+});
+
+app.get('/', function (req, res) {
+
+   res.render('Email');
+
+});
+
+app.listen(4000);
