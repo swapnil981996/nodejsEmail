777f6fb3b64e8a3917e7300928de0bb9b0cf2f0b
swapnil <swapnilgandal8@gmail.com>
2020-07-09 11:20:56 +0530

    completed task

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
+
 var app = express();
 var PORT=process.env.PORT || 4000
+
+var transporter = nodemailer.createTransport({
+   service: 'gmail',
+   auth: {
+     user: 'swapnilgandal9@gmail.com', //your email address
+     pass: 'SwapGandal9' //your password
+   }
+ });
+
 app.set("view engine","jade")
 
+app.get('/', function (req, res) {
+
+   res.render('Email');
+
+});
+
 app.get('/send-email', function(req, res){
-   console.log(req.query.toEmail)
+   //regular expression for validating email 
    const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-   if(req.query.toEmail)
+   var recipient_email=req.query.toEmail
+
+   var mailOptions = {
+      from: 'swapnilgandal9@gmail.com', //your email address
+      to: recipient_email,  // recipient email address
+      subject: 'About your health', // subject of the mail
+      text: 'Stay Home, Stay Safe, Stay Strong!!!!!' // text or content of the mail
+    };
+
+   if(recipient_email)
    {
-      if(emailRegexp.test(req.query.toEmail))
+      if(emailRegexp.test(recipient_email))
       {
-         res.render('Email',{ 'msg': 'mail sent' });
+         transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              res.render('Email',{ 'msg': 'something went wrong' })
+            } else {
+              res.render('Email',{ 'msg': 'mail sent' });
+            }
+          });
       }
       else
       {
          res.render('Email',{ 'msg': 'invalid email address' });
-      }
-      
+      } 
    }
    else
    {
@@ -24,10 +55,5 @@ app.get('/send-email', function(req, res){
    }
 });
 
-app.get('/', function (req, res) {
-
-   res.render('Email');
-
-});
 
 app.listen(PORT);
